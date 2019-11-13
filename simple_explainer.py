01a1908ab57858ca24ce53d72dab602b461479f4
vu-minh <vuvietminh@unamur.be>
2019-11-13 23:26:11 +0100

    rename simple explainer, test with Lasso/ElasticNet, refine heatmap ploting

--- simple_explainer.py
+++ simple_explainer.py
@@ -11,44 +11,12 @@ from matplotlib import pyplot as plt
 from sklearn.utils import shuffle
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Lasso, ElasticNet
 
 from common.dataset import dataset
-from sample_tsne import tsne_sample_embedded_points, plot_with_samples
-from sample_tsne import perturb_image, remove_blob
-
-
-def plot_samples(samples, out_name="noname01"):
-    """Plot sampled images in a grid of subplots
-    """
-    img_size = int(math.sqrt(samples.shape[1]))
-    n_rows = n_cols = math.ceil(math.sqrt(samples.shape[0]))
-    fig, axes = plt.subplots(n_rows, n_cols, figsize=(n_cols * 2, n_rows * 2))
-
-    for ax in axes.ravel():
-        ax.axis("off")
-
-    for img, ax in zip(samples, axes.ravel()):
-        ax.imshow(img.reshape(img_size, img_size), cmap="binary")
-
-    fig.savefig(out_name)
-    plt.close(fig)
-
-
-def plot_heatmap(W, out_name="noname00"):
-    """Plot heatmap to visualize the weights `W`
-    Args:
-        W: [2xD], D: dimensionality of input image, which is `img_size x img_size`
-    """
-    n_targets = len(W)
-    img_size = int(math.sqrt(W.shape[1]))
-    fig, axes = plt.subplots(1, n_targets, figsize=(6 * n_targets, 4))
-
-    for i, ax in enumerate([axes] if 1 == n_targets else axes.ravel()):
-        ax.axis("off")
-        im = ax.imshow(W[i, :].reshape(img_size, img_size), cmap="RdBu")
-        fig.colorbar(im, ax=ax)
-    fig.savefig(out_name)
-    plt.close(fig)
+from sample_tsne import tsne_sample_embedded_points
+from sampling import perturb_image, remove_blob
+from utils import scatter_with_samples, plot_samples, plot_heatmap
 
 
 def explain_samples(x_samples, y_samples):
@@ -61,9 +29,9 @@ def explain_samples(x_samples, y_samples):
         weights of LR model (representing the importance of each pixel)
     """
     # x_samples = StandardScaler().fit_transform(x_samples)
-
     # x_samples = x_samples - x_samples.mean(axis=0, keepdims=True)
-    reg = LinearRegression(fit_intercept=False).fit(x_samples, y_samples)
+
+    reg = ElasticNet(alpha=0.5, l1_ratio=0.75).fit(x_samples, y_samples)
     return reg.coef_
 
 
@@ -75,31 +43,17 @@ def test_remove_blob(x, n_repeat=16, n_remove=1):
     plot_samples(np.array(x_samples), out_name=f"{plot_dir}/test_remove_blob{n_remove}.png")
 
 
-if __name__ == "__main__":
-    n_samples = 50  # number of points to sample
-    sigma_HD = 0.5  # larger of Gaussian in HD
-    sigma_LD = 0.5  # larger of Gaussian in LD
-    seed = 4096  # for reproducing
-    debug_level = 0  # verbose in tsne
-    N_max = 200  # maximum number of data points for testing only
-    force_recompute = False  # use pre-calculated embedding and samples or recompute them
-
-    # sampling method in ["sample_around", "sample_with_global_noise", "perturb_image", "remove_blob"]
-    sampling_method = "remove_blob"
-    data_home = "./data"  # local data on my computer
-    dataset_name = "FASHION200"
-    plot_dir = f"./plots/{dataset_name}"
-    log_dir = f"./var/{dataset_name}"
-    for a_dir in [plot_dir, log_dir]:
-        if not os.path.exists(a_dir):
-            os.mkdir(a_dir)
+def run_explainer():
+    """Test workflow: generate samples, apply a linear model and try to explain the weights
+    Note: using global variables in __main__ (not good practice)
+    """
 
     # TODO check stability of sampling. (e.g. seed 1024 iris, 42 digits)
     # set numpy random seed for reproducing
     np.random.seed(seed)
 
     # basic params to run tsne the first time
-    tsne_hyper_params = dict(perplexity=30, n_iter=1500, random_state=seed, verbose=debug_level)
+    tsne_hyper_params = dict(perplexity=50, n_iter=1500, random_state=seed, verbose=debug_level)
 
     # to re-run tsne quickly, take the initial embedding as `init` and use the following params
     early_stop_hyper_params = dict(
@@ -113,11 +67,6 @@ if __name__ == "__main__":
     dataset.set_data_home(data_home)
     X_original, X, labels = dataset.load_dataset(dataset_name)
     X_original, X, labels = shuffle(X_original, X, labels, n_samples=N_max, random_state=seed)
-
-    # test_remove_blob(X[89], n_remove=1)
-    # test_remove_blob(X[0], n_remove=2)
-    # test_remove_blob(X[123], n_remove=3)
-    # import sys; sys.exit(0)
 
     # select a point to sample in HD
     selected_idx = np.random.randint(X.shape[0])
@@ -140,11 +89,38 @@ if __name__ == "__main__":
     # viz the original embedding with the new sampled points
     out_name_prefix = f"{plot_dir}/{sigma_HD}-{sigma_LD}-{n_samples}"
     out_name = f"{out_name_prefix}_scatter.png"
-    plot_with_samples(Y, y_samples, selected_idx, labels=labels, out_name=out_name)
+    scatter_with_samples(Y, y_samples, selected_idx, labels=labels, out_name=out_name)
 
     # plot the weights of the linear model
     W = explain_samples(x_samples, y_samples)
-    plot_heatmap(W, out_name=f"{out_name_prefix}_explanation.png")
+    plot_heatmap(W, img=X_original[selected_idx], out_name=f"{out_name_prefix}_explanation.png")
 
     # show the sampled images in HD
     plot_samples(x_samples, out_name=f"{out_name_prefix}_samples.png")
+
+
+if __name__ == "__main__":
+    # TODO: turn these variables to input arguments with argparse
+    n_samples = 10  # number of points to sample
+    sigma_HD = 0.5  # larger of Gaussian in HD
+    sigma_LD = 0.5  # larger of Gaussian in LD
+    seed = 8888  # for reproducing
+    debug_level = 0  # verbose in tsne
+    N_max = 500  # maximum number of data points for testing only
+    force_recompute = False  # use pre-calculated embedding and samples or recompute them
+
+    # sampling method in ["sample_around", "sample_with_global_noise", "perturb_image", "remove_blob"]
+    sampling_method = "remove_blob"
+    data_home = "./data"  # local data on my computer
+    dataset_name = "MNIST"
+    plot_dir = f"./plots/{dataset_name}"
+    log_dir = f"./var/{dataset_name}"
+    for a_dir in [plot_dir, log_dir]:
+        if not os.path.exists(a_dir):
+            os.mkdir(a_dir)
+
+    run_explainer()
+
+    # test_remove_blob(X[89], n_remove=1)
+    # test_remove_blob(X[0], n_remove=2)
+    # test_remove_blob(X[123], n_remove=3)
