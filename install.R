eb3feb4e0fb7be79bb05164fbdd73175cedc8317
vu-minh <vuvietminh@unamur.be>
2021-11-12 09:36:30 +0000

    add script to install R and dependent R packages

---
+++ install.R
@@ -0,0 +1,6 @@
+install.packages("glmnet")
+install.packages("pbapply")
+install.packages("GenSA")
+install.packages("exactRankTests")
+
+# R -e 'install.packages(c("glmnet", "pbapply", "GenSA", "exactRankTests"))'
